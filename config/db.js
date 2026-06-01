10e5febef59412f62db3aec582e60029f175fdfb
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-06-01 20:35:34 +0530

    fix: changes undefined res to console in db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const dbConnection=async()=>{
-    try{
+const dbConnection = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Mongodb connected successfully");
     }
-    catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message
-        })
+    catch (error) {
+        console.error("Database connection failed:", error)
+        process.exit(1)
     }
 }
+
 export default dbConnection
