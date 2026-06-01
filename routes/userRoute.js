1ca049793cb80ec80aab9d862295a833b1b2fe6b
Anant Tripathi <gammadeveloper5114@gmail.com>
2026-06-02 00:20:39 +0530

    feat: registered login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,12 @@ import express from 'express';
 import { register } from '../controller/userController.js';
 import { verifyEmailController } from '../controller/verifyEmail.js';
 
-export const router=express.Router();
+export const router = express.Router();
 
 router.post("/register", register);
 router.get("/verify-email", verifyEmailController);
+router.post("/login", login); //added login routes here
+
+
+
+
